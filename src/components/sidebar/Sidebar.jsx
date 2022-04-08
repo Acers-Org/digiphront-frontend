7bbbadbbc9692f5b32e4c1cc7a0a0884dd08ee8a
Gbemisola Kotoye <barkty@yahoo.com>
2022-04-08 09:39:42 +0100

    map data

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,14 +1,17 @@
 import './Sidebar.css';
-import { SidebarData } from './SidebarData';
+import { SidebarData, TeacherSideBar } from './SidebarData';
 import logo from '../../assets/logo.svg'
+import { useState } from 'react';
 
 
 function Sidebar() {
+  const [user, setUser] = useState('Student')
+
   return (
     <div className='sidebar'>
         <img src={logo} alt="logo" />
         <ul className= 'sidebar-list'>
-            {SidebarData.map((val, key) => {
+            {user === 'Teacher' ? (TeacherSideBar.map((val, key) => {
                 return (
                     <li 
                         className='list-item'
@@ -19,7 +22,18 @@ function Sidebar() {
                     <div>{val.title}</div>
                     </li>
                     )
-                })}
+                })) : (SidebarData.map((val, key) => {
+                  return (
+                      <li 
+                          className='list-item'
+                          id={window.location.pathname === val.link ? 'active': ''}
+                          key={key} 
+                          onClick={() => {window.location.pathname = val.link}}>
+                      
+                      <div>{val.title}</div>
+                      </li>
+                      )
+                  }))}
         </ul>
     </div>
 
--- src/components/sidebar/SidebarData.js
+++ src/components/sidebar/SidebarData.js
@@ -39,3 +39,96 @@ export const SidebarData = [
     }
 ]
 
+export const TeacherSideBar = [
+    {
+        title: 'Dashboard',
+        link: '/dashboard'
+    },
+    {
+        title: 'Inbox',
+        link: '/inbox'
+    },
+    {
+        title: 'My Classes',
+        link: '/myclasses'
+    },
+    {
+        title: 'Groups',
+        link: '/groups'
+    },
+    {
+        title: 'Exams/Result',
+        link: '/exams-result'
+    },
+    {
+        title: 'Students',
+        link: '/students'
+    },
+    {
+        title: 'Files',
+        link: '/files'
+    },
+    {
+        title: 'Settings',
+        link: '/settings'
+    },
+    {
+        title: 'Log Out',
+        link: '/logout'
+    }
+]
+
+export const AdminSideBar = [
+    {
+        title: 'Dashboard',
+        link: '/dashboard'
+    },
+    {
+        title: 'Students',
+        link: '/students'
+    },
+    {
+        title: 'Teachers',
+        link: '/teachers'
+    },
+    {
+        title: 'Parents',
+        link: '/parents'
+    },
+    {
+        title: 'Exams/Results',
+        link: '/exams'
+    },
+    {
+        title: 'Notice Board',
+        link: '/noticeboard'
+    },
+    {
+        title: 'Message',
+        link: '/messsage'
+    },
+    {
+        title: 'Classes',
+        link: '/classes'
+    },
+    {
+        title: 'Registration',
+        link: '/registration'
+    },
+    {
+        title: 'Attendance',
+        link: '/attendance'
+    },
+    {
+        title: 'Transactions',
+        link: '/transactions'
+    },
+    {
+        title: 'Settings',
+        link: '/settings'
+    },
+    {
+        title: 'Log Out',
+        link: '/logout'
+    }
+]
