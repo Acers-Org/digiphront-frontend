43d8800e40dee87dc583d5328bb12bad02376407
Kotoye Gbemisola <58271499+Barkty@users.noreply.github.com>
2022-04-13 19:20:50 +0100

    update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,22 +1,18 @@
-import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, Navigate} from "react-router-dom";
 import useContextGetter from "../hooks/useContextGetter";
-import { NoAuthURL } from '../store/NoAuthURL';
+//import { NoAuthURL } from '../store/NoAuthURL';
 
-function ProtectedRoute({ Component, routePath, ...rest }) {
+function ProtectedRoute() {
     const auth = useContextGetter();
     const isAuth = auth.isAuth;
     const token = auth.user.token;
-    const navigate = useNavigate();
-    const location = useLocation();
-    const path = location.pathname.split('/');
+    //const navigate = useNavigate();
+    //const location = useLocation();
+    //const path = location.pathname.split('/');
     
   return (
     <div>
-        <Routes>
-        {isAuth && token ? (
-        <Route {...rest} path={routePath} element={<Component/>}/>) 
-        : (NoAuthURL.includes(`/${path}`)) ? navigate(`/${path}`) : navigate('/login')}
-        </Routes>
+        {!isAuth && !token ? (<Navigate to='/login') : (<Navigate to='/')}
     </div>
   );
 }
