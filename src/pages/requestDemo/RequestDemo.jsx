4b178e2c24b1581cff8b2a5009612595306ba400
Remilekun Odegbami <remdev22@gmail.com>
2022-04-18 15:17:08 +0100

    added request demo page

--- src/pages/requestDemo/RequestDemo.jsx
+++ src/pages/requestDemo/RequestDemo.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './RequestDemo.css'
 import { Link, useNavigate } from 'react-router-dom';
 import { useFormik } from 'formik';
 import { Spinner } from 'react-bootstrap';
@@ -51,8 +52,14 @@ function RequestDemo() {
     }    
     const formik = useFormik({
         initialValues: {
+            name: '',
             email: '',
+            number: '',
             password: '',
+            location: '',
+            instituteName: '',
+            role: '',
+            website: '',
             user: {
                 student: true,
                 admin: false,
@@ -73,45 +80,114 @@ function RequestDemo() {
                 </div>
 
                 <form action="" className='form w-75' onSubmit={formik.handleSubmit}>
-                        <div className='d-flex flex-column'>
-                            <label htmlFor="email">Email</label>
-                            <input type="email" 
-                            name="email" 
-                            id="email" 
-                            value={formik.values.email}
-                            onChange={formik.handleChange}
-                            />
-                            {formik.errors.email && formik.touched.email ? (
-                                <span className='error'>{formik.errors.email}</span>
-                            ) : null}
+                        <div className='d-flex justify-content-between'>
+                            <div className="d-flex flex-column">
+                                <label htmlFor="name">Name</label>
+                                <input type="text" 
+                                name="name" 
+                                id="name" 
+                                value={formik.values.name}
+                                onChange={formik.handleChange}
+                                />
+                                {formik.errors.name && <formik className="touched name"></formik> ? (
+                                    <span className='error'>{formik.errors.name}</span>
+                                ) : null}
+                            </div>
+                            <div className="d-flex flex-column">
+                                <label htmlFor="email">Email Address</label>
+                                <input type="email" 
+                                name="email" 
+                                id="email" 
+                                value={formik.values.email}
+                                onChange={formik.handleChange}
+                                />
+                                {formik.errors.email && formik.touched.email ? (
+                                    <span className='error'>{formik.errors.email}</span>
+                                ) : null}
+                            </div>
                         </div>
+                        <div className='d-flex justify-content-between pt-5'>
+                            <div className="d-flex flex-column">
+                                <label htmlFor="number">Mobile Number</label>
+                                <input type="number" 
+                                name="number" 
+                                id="number" 
+                                value={formik.values.number}
+                                onChange={formik.handleChange}
+                                />
+                                {formik.errors.number && <formik className="touched name"></formik> ? (
+                                    <span className='error'>{formik.errors.number}</span>
+                                ) : null}
+                            </div>
+                            <div className="d-flex flex-column">
+                                <label htmlFor="location">Location</label>
+                                <input type="location" 
+                                name="location" 
+                                id="location" 
+                                value={formik.values.location}
+                                onChange={formik.handleChange}
+                                />
+                                {formik.errors.location && formik.touched.location ? (
+                                    <span className='error'>{formik.errors.location}</span>
+                                ) : null}
+                            </div>
+                        </div>
+                        <div className="mb-2 select">
+                            <label htmlFor="select" className="form-label">Select a plan</label>                                        
+                            <select className="form-select" aria-label="Default select example"required>
+                                <option value="basic" defaultValue={"Basic"} id="select" >Basic</option>
+                                <option value="premium" id="select" >Premium</option>
+                            </select>
+                        </div>
+                        <div className='d-flex justify-content-between'>
+                            <div className="d-flex flex-column">
+                                <label htmlFor="institution-name">Institution Name</label>
+                                <input type="name" 
+                                name="name" 
+                                id="institution-name" 
+                                value={formik.values.instituteName}
+                                onChange={formik.handleChange}
+                                />
+                                {formik.errors.instituteName && <formik className="touched name"></formik> ? (
+                                    <span className='error'>{formik.errors.instituteName}</span>
+                                ) : null}
+                            </div>
+                            <div className="d-flex flex-column">
+                                <label htmlFor="role">Your role</label>
+                                <input type="text" 
+                                name="role" 
+                                id="role"
+                                placeholder='e.g Principal, IT Administrator...'
+                                value={formik.values.role}
+                                onChange={formik.handleChange}
+                                />
+                                {formik.errors.role && <formik className="touched name"></formik> ? (
+                                    <span className='error'>{formik.errors.role}</span>
+                                ) : null}
+                            </div>                            
+                        </div>
+                            <div className="d-flex flex-column">
+                                <label htmlFor="role">institution Website</label>
+                                <input type="text" 
+                                name="website" 
+                                id="website"
+                                value={formik.values.website}
+                                onChange={formik.handleChange}
+                                />
+                                {formik.errors.website && <formik className="touched name"></formik> ? (
+                                    <span className='error'>{formik.errors.website}</span>
+                                ) : null}
+                            </div>                            
                         <div className="d-flex flex-column">
-                            <label htmlFor="password">Password</label>
-                            <input type="password" 
-                            name="password" 
-                            id="password"
-                            value={formik.values.password}
-                            onChange={formik.handleChange}
-                            />
-                            {formik.errors.password && formik.touched.password ? (
-                            <span className='error'>{formik.errors.password}</span>
+                            <label htmlFor="message">Message</label>
+                            <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                            {formik.errors.message && formik.touched.message ? (
+                            <span className='error'>{formik.errors.message}</span>
                             ) : null}
                         </div>
-                        <button className="btn text-uppercase btn-block btn-primary w-100 mt-3" type='submit' disabled={formik.isSubmitting} >
+                        <button className="submit btn text-uppercase mt-3" type='submit' disabled={formik.isSubmitting} >
                             {!formik.isSubmitting ? ("Sign Up") : (<Spinner animation="border" variant="light"/>)}
                         </button>
-
-                        <div className="d-flex justify-content-between mt-3 align">
-                            <div className="checkbox">
-                                <input type="checkbox" 
-                                name="rememberMe" 
-                                id="rememberMe" 
-                                value={formik.values.rememberMe}
-                                onChange={formik.handleChange} />
-                                <label htmlFor="rememberMe"> Remember me</label>
-                            </div>
-                            <h5> <Link to="/" className='text-capitalize'> Forgot password?</Link> </h5>
-                        </div>
                     </form>
             </div>
         </div>
