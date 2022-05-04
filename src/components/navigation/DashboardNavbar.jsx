5414b0376b57aa02f98828837d3630646be0fd91
Evelyn Anyebe <anyebeevelyn@gmail.com>
2022-05-04 15:10:30 +0100

    edit student dashboard

--- src/components/navigation/DashboardNavbar.jsx
+++ src/components/navigation/DashboardNavbar.jsx
@@ -42,7 +42,38 @@ const DashboardNavbar = () => {
           </ul>
         </>
       ) : user?.student.isStudent ? (
-        <></>
+        <>
+          <span>&nbsp;</span>
+          <ul className={styles.dashboardnav__navbar}>
+            <li className={styles.dashboardnav__navbar__link}>
+              <NavLink to="/">
+                {" "}
+                <i
+                  className={`fa fa-bell ${styles.dashboardnav__navbar__link__notificationicon}`}
+                ></i>
+                <span
+                  className={styles.dashboardnav__navbar__link__notifications}
+                >
+                  5
+                </span>
+              </NavLink>
+            </li>
+            <li className={styles.dashboardnav__navbar__link}>
+              <NavLink to="/">
+                <span>
+                  <strong>{user.firstname + " " + user.lastname}</strong>
+                  <br />
+                  Student
+                </span>
+                <img
+                  src="https://images.pexels.com/photos/6325984/pexels-photo-6325984.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                  alt="user"
+                  className={`${styles.dashboardnav__navbar__link__usericon}`}
+                ></img>{" "}
+              </NavLink>
+            </li>
+          </ul>
+        </>
       ) : user?.admin.isAdmin ? (
         <>
           <div className={styles.dashboardnav__search}>
