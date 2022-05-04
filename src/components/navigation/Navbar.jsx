d79cf6e533c4854d4bf6c22f31348ab6ab28a73e
Gbemisola Kotoye <barkty@yahoo.com>
2022-05-04 07:21:52 +0100

    not responsive navbar

--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,15 +1,24 @@
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink, Link, useLocation } from 'react-router-dom';
 import { Logo } from '../logo/logo';
 import styles from './Navbar.module.scss'
 import style from '../hero/Hero.module.scss'
+import { useState } from 'react';
+import { HiOutlineMenuAlt1 } from 'react-icons/hi'
+import { FaTimes } from 'react-icons/fa'
 
 const Navbar = () => {
+    const location = useLocation()
+  const [isMobile, setIsMobile] = useState(false);
+  const [active, setActive] = useState(location.pathname);
+  const handleActive = (menu) => {
+    setActive(menu);
+  };
 
     return (
         <div className={styles.wrapper}>
             <Logo/>
-            <nav className={styles.wrapper__navbar}>
-                <ul className={styles.wrapper__navbar__links}>
+            <nav className={isMobile ? styles.wrapper__navbar : styles.wrapper__close}>
+                <ul className={isMobile ? styles.wrapper__navbar__links : styles.wrapper__navbar__close}>
                     <li className={styles.wrapper__navbar__links__link}>
                         <NavLink to='/features'>Features</NavLink>
                     </li>
@@ -32,6 +41,11 @@ const Navbar = () => {
                     </li>
                 </ul>
             </nav>
+            <button className={`${styles.mobile_menu_icon}`} onClick={()=>setIsMobile(!isMobile)}>
+                {isMobile ? (
+                <FaTimes className={`${styles.mobile_icon}`}/>) : (
+                <HiOutlineMenuAlt1 className={`${styles.mobile_icon}`}/>)}
+            </button>
         </div>
     )
 }
